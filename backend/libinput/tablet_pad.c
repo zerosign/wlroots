c86ad7b19e4e10509eef3df7f9f4d656258017d5
illiliti <illiliti@protonmail.com>
2022-03-13 22:20:56 +0300

    backend/libinput/tablet_pad.c: explicitly include libudev

--- backend/libinput/tablet_pad.c
+++ backend/libinput/tablet_pad.c
@@ -3,6 +3,7 @@
 #include <string.h>
 #include <libinput.h>
 #include <stdlib.h>
+#include <libudev.h>
 #include <wlr/interfaces/wlr_tablet_pad.h>
 #include <wlr/util/log.h>
 #include "backend/libinput.h"
