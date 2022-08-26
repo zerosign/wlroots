09a7b64852a358403c7eeb94f01d88c481b01733
Alexander Orzechowski <orzechowski.alexander@gmail.com>
2022-08-25 21:55:46 -0400

    util/array: Add reverse helpers

--- include/util/array.h
+++ include/util/array.h
@@ -15,4 +15,15 @@ void array_remove_at(struct wl_array *arr, size_t offset, size_t size);
  */
 bool array_realloc(struct wl_array *arr, size_t size);
 
+/**
+ * Returns a pointer to the first valid element in a reversed array.
+ */
+void *array_reversed_start(struct wl_array *arr);
+
+/**
+ * Adds a new element to the array inserting them starting from a higher
+ * memory address effectively inserting them in reverse order.
+ */
+void *array_reversed_add(struct wl_array *arr, size_t size);
+
 #endif
