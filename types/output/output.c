7a89b074598b97c8500cd8d508098a59729f2154
Alexander Orzechowski <alex@ozal.ski>
2023-01-04 00:45:26 -0500

    output: destroy global after destroy events
    This lets the compositor do stuff with the global right before destruction.

--- types/output/output.c
+++ types/output/output.c
@@ -386,11 +386,12 @@ void wlr_output_destroy(struct wlr_output *output) {
 	}
 
 	wl_list_remove(&output->display_destroy.link);
-	wlr_output_destroy_global(output);
 	output_clear_back_buffer(output);
 
 	wl_signal_emit_mutable(&output->events.destroy, output);
 	wlr_addon_set_finish(&output->addons);
+
+	wlr_output_destroy_global(output);
 
 	// The backend is responsible for free-ing the list of modes
 
