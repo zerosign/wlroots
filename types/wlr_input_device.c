179d6efb191d5aab63c02d35eb1f6756a387266f
Kirill Primak <vyivel@eclair.cafe>
2023-01-18 02:07:57 +0300

    input-device: add addons

--- types/wlr_input_device.c
+++ types/wlr_input_device.c
@@ -2,6 +2,7 @@
 
 #include <stdlib.h>
 #include <string.h>
+#include <wlr/util/addon.h>
 #include "interfaces/wlr_input_device.h"
 
 void wlr_input_device_init(struct wlr_input_device *dev,
@@ -11,6 +12,7 @@ void wlr_input_device_init(struct wlr_input_device *dev,
 		.name = strdup(name),
 	};
 
+	wlr_addon_set_init(&dev->addons);
 	wl_signal_init(&dev->events.destroy);
 }
 
@@ -21,6 +23,7 @@ void wlr_input_device_finish(struct wlr_input_device *wlr_device) {
 
 	wl_signal_emit_mutable(&wlr_device->events.destroy, wlr_device);
 
+	wlr_addon_set_finish(&wlr_device->addons);
 	wl_list_remove(&wlr_device->events.destroy.listener_list);
 
 	free(wlr_device->name);
